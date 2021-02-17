2d760fb5492b50f242bd6563ec987d2dd4fa71df
Pravesh Kunwar <praveshk04@outlook.com>
2021-02-16 20:31:01 -0500

    asd

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Link, Switch, Router, Route } from "react-router-dom";
-import AboutMe from "../main/AboutMe";
-import Contact from "../main/Contact";
+import AboutMe from "../../pages/AboutMe";
+import Contact from "../../pages/Contact";
 
 const Navbar: React.FC = () => {
   return <div className="navbar"></div>;
