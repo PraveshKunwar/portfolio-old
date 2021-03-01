cdc84d6a7aa941ec63dafc5812b1b42a1b1cae2f
Pravesh Kunwar <praveshk04@outlook.com>
2021-02-28 23:15:21 -0500

    Finishing projects section soon. Need to fix typography

--- src/components/section_3/Projects.tsx
+++ src/components/section_3/Projects.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ProjectHeader } from '../../styled-components/Header';
 import { Box } from '../../styled-components/Box';
+import Link from '../../styled-components/Link';
 import Colors from '../../utils/Colors';
 import GithubProjects from '../../utils/GithubProjects';
 import { Git } from '../../assets/svg/SVG_Icons';
@@ -10,7 +11,7 @@ const Projects: React.FC = () => {
 		<div className="?projects">
 			<ProjectHeader
 				id="projects"
-				color={Colors.pink_main}
+				color={Colors.dark_slate_border_purple}
 				size="30px"
 				top="2rem"
 				weight={700}
@@ -21,13 +22,21 @@ const Projects: React.FC = () => {
 			<Box
 				width="400px"
 				height="300px"
-				background={Colors.lighter_navy}
+				background={Colors.pink_main}
 				color={Colors.dark_slate_border_purple}
 				size="16px"
 				bottom="4rem"
+				radius="0px"
 			>
-				<div>
-					<Git />
+				<div className="?projects_socialistic">
+					<Link
+						color=""
+						href={GithubProjects.socialistic.link}
+						rel="noreferrer"
+						target="_blank"
+					>
+						<Git />
+					</Link>
 					<ProjectHeader
 						id="projects"
 						color={Colors.gold}
@@ -38,6 +47,102 @@ const Projects: React.FC = () => {
 					>
 						Socialistic
 					</ProjectHeader>
+					<p style={{ color: Colors.lighter_pink }}>
+						<b style={{ color: Colors.slate }}>▹ </b>
+						{GithubProjects.socialistic.description} (Coming Soon!)
+					</p>
+					<p style={{ color: Colors.lighter_pink }}>
+						<b style={{ color: Colors.slate }}>▹ </b>
+						<b>Languages: </b>
+						{GithubProjects.socialistic.languages}
+					</p>
+				</div>
+				<div className="?projects_ultimatum">
+					<Link
+						color=""
+						href={GithubProjects.ultimatum.link}
+						rel="noreferrer"
+						target="_blank"
+					>
+						<Git />
+					</Link>
+					<ProjectHeader
+						id="projects"
+						color={Colors.gold}
+						size="20px"
+						top="-1.5rem"
+						weight={700}
+						bottom="2rem"
+					>
+						Ultimatum
+					</ProjectHeader>
+					<p style={{ color: Colors.lighter_pink }}>
+						<b style={{ color: Colors.slate }}>▹ </b>
+						{GithubProjects.ultimatum.description} (Coming Soon!)
+					</p>
+					<p style={{ color: Colors.lighter_pink }}>
+						<b style={{ color: Colors.slate }}>▹ </b>
+						<b>Languages: </b>
+						{GithubProjects.ultimatum.languages}
+					</p>
+				</div>
+				<div className="?projects_statistify">
+					<Link
+						color=""
+						href={GithubProjects.statistify.link}
+						rel="noreferrer"
+						target="_blank"
+					>
+						<Git />
+					</Link>
+					<ProjectHeader
+						id="projects"
+						color={Colors.gold}
+						size="20px"
+						top="-1.5rem"
+						weight={700}
+						bottom="2rem"
+					>
+						Statistify
+					</ProjectHeader>
+					<p style={{ color: Colors.lighter_pink }}>
+						<b style={{ color: Colors.slate }}>▹ </b>
+						{GithubProjects.statistify.description} (Coming Soon!)
+					</p>
+					<p style={{ color: Colors.lighter_pink }}>
+						<b style={{ color: Colors.slate }}>▹ </b>
+						<b>Languages: </b>
+						{GithubProjects.statistify.languages}
+					</p>
+				</div>
+				<div className="?projects_spotifibe">
+					<Link
+						color=""
+						href={GithubProjects.spotifibe.link}
+						rel="noreferrer"
+						target="_blank"
+					>
+						<Git />
+					</Link>
+					<ProjectHeader
+						id="projects"
+						color={Colors.gold}
+						size="20px"
+						top="-1.5rem"
+						weight={700}
+						bottom="2rem"
+					>
+						Spotifibe
+					</ProjectHeader>
+					<p style={{ color: Colors.lighter_pink }}>
+						<b style={{ color: Colors.slate }}>▹ </b>
+						{GithubProjects.spotifibe.description} (Coming Soon!)
+					</p>
+					<p style={{ color: Colors.lighter_pink }}>
+						<b style={{ color: Colors.slate }}>▹ </b>
+						<b>Languages: </b>
+						{GithubProjects.spotifibe.languages}
+					</p>
 				</div>
 			</Box>
 		</div>
--- src/utils/Colors.ts
+++ src/utils/Colors.ts
@@ -1,11 +1,12 @@
 const Colors = {
 	navy_main: '#0a192f',
 	lighter_navy: '#112240',
+	lighter_pink: '#ddc1e0',
 	blue_main: 'rgb(21, 114, 182)',
 	pink_main: '#b52660',
 	turqoise_main: '#53c0c9',
 	slate: '#ccd6f6',
-	gold: '#c7cc43',
+	gold: '#FFD700',
 	dark_slate_border_purple: '#7e8dbd',
 };
 
