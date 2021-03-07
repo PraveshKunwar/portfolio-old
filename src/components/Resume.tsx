d88fba09527670ebffb8b4e7c7a3c2c4f7a67d07
Pravesh Kunwar <praveshk04@outlook.com>
2021-03-06 20:46:47 -0500

    working

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import resume from '../static/resume.pdf';
 import { Document, Page } from 'react-pdf';
+
 const Resume: React.FC = () => {
 	const [numPages, setNumPages] = useState(null);
 	const [pageNumber, setPageNumber] = useState(1);
@@ -9,7 +9,7 @@ const Resume: React.FC = () => {
 	};
 	return (
 		<div className="#resume">
-			<Document file={resume} onLoadSuccess={onSuccess}>
+			<Document onLoadSuccess={onSuccess}>
 				{' '}
 				<Page pageNumber={pageNumber} />
 			</Document>
