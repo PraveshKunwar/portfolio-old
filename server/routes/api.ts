cb9fd9274f6eb868fb865186fa08518a83d75434
Pravesh Kunwar <praveshk04@outlook.com>
2021-03-07 22:05:55 -0500

    adding graphql :o

--- server/routes/api.ts
+++ server/routes/api.ts
@@ -1,6 +1,37 @@
 import express, { Request, Response, Router } from 'express';
+import { GITHUB } from '../secrets';
+import fetch from 'node-fetch';
 const router: Router = express.Router();
+require('dotenv').config();
 
-router.get('/dummy', (req: Request, res: Response) => {});
+const token = process.env.GITHUB_TOKEN || GITHUB;
+const query = `
+	query {
+			repository(name: "Ultimatum", owner: "PraveshKunwar") {
+				defaultBranchRef {
+					target {
+						... on Commit {
+							history(first: 1) {
+								nodes {
+									message
+									committedDate
+									authoredDate
+									oid
+									author {
+										email
+										name
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+	}
+`;
+router.get('/dummy', (req: Request, res: Response) => {
+	res.send('hello world');
+});
+router.get('/github', (req: Request, res: Response) => {});
 
 module.exports = router;
