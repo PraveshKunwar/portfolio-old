ce851e829dacb115a0747b8b27bd5c75c9b7297a
Pravesh Kunwar <praveshk04@outlook.com>
2021-03-22 16:17:53 -0400

    dist folder public for no reason

--- dist/js/client/src/utils/Socials.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Socials = {
-    github: "https://github.com/PraveshKunwar",
-    instagram: "https://www.instagram.com/praveshk_/",
-    youtube: "https://www.youtube.com/channel/UCqSeLKlJkKgZNf8bEu_7Dlw",
-    twitter: "https://twitter.com/PraveshK_",
-};
-exports.default = Socials;
